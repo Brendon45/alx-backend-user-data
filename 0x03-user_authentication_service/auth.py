c604908acdebdd57ec37939a38f79f2389efdfa4
Brendon45 <jejebrendon722@gmail.com>
2024-08-14 13:44:07 +0200

    updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,3 +19,25 @@ def _hash_password(password: str) -> bytes:
     hashed_password = hashpw(password.encode('utf-8'), gensalt())
 
     return hashed_password
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """registers a new user"""
+        try:
+            # Find the user with the given email
+            user = self._db.find_user_by(email=email)
+            if user:
+                # If user already exists, throw error
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password)
+            # Add user to database
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
