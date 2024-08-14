107928461e14dc6ce55d2b5ff21e5fc939da0d04
Brendon45 <jejebrendon722@gmail.com>
2024-08-14 13:55:59 +0200

    updated

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module app
+"""
+from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
+
+app = Flask(__name__)
+AUTH = Auth()
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def basic() -> str:
+    """
+    Returns a basic json response
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register() -> str:
+    """ Register a new user """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        if user is not None:
+            return jsonify({
+                "email": user.email,
+                "message": "user created"
+                })
+    except ValueError:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
