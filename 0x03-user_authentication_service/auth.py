68f9583d3aa27d5ba81219133f4fd94e84c3e97a
Brendon45 <jejebrendon722@gmail.com>
2024-08-14 19:53:55 +0200

    Solutions for all completed tasks

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,3 +88,16 @@ class Auth:
         self._db.update_user(user.id, session_id=session_id)
 
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> Union[str, None]:
+        """
+        Finds a user using the session id
+        """
+        if session_id is None:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
